1fa0dd762edbe727986fb266aef78e69bf97b1d4
Benjamin <benjminrichie@gmail.com>
2023-10-14 03:15:24 -0700

    update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,13 +3,13 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - this is a printf function
+ * @format: This is format.
+ * Return: chars.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int d, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -19,25 +19,25 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (d = 0; format && format[d] != '\0'; d++)
 	{
-		if (format[i] != '%')
+		if (format[d] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = format[d];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+			/* write(1, &format[d], 1);*/
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
+			flags = get_flags(format, &d);
+			width = get_width(format, &d, list);
+			precision = get_precision(format, &d, list);
+			size = get_size(format, &d);
+			++d;
+			printed = handle_print(format, &d, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -53,9 +53,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - This will print the content of a buffer
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: This is the index to add next char snd represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
--- print_int.c
+++ print_int.c
@@ -8,7 +8,7 @@
 
 int printf_int(va_list args)
 {
-	int n = va_args(args, int);
+	int n = va_arg(args, int);
 	int num, last = n % 10, digit, exp = 1;
 	int q = 1;
 
