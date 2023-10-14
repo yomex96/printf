cdd904bd3599a566e4e1ca154517e12fd3a1db39
Benjamin <benjminrichie@gmail.com>
2023-10-14 05:43:48 -0700

    update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * get_flags - this function simply calculates active flags
+ * @format: This is the formatted string to print the arguments
+ * @d: This will take a parameter.
+ * Return: The flags:
  */
-int get_flags(const char *format, int *i)
+int get_flags(const char *format, int *d)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int j, curr_i;
+	int e, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_i = *d + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+		for (e = 0; FLAGS_CH[e] != '\0'; e++)
+			if (format[curr_i] == FLAGS_CH[e])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= FLAGS_ARR[e];
 				break;
 			}
 
-		if (FLAGS_CH[j] == 0)
+		if (FLAGS_CH[e] == 0)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*d = curr_i - 1;
 
 	return (flags);
 }
