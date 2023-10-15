e122e7e3716f43cba81250d978da3b3a0b1dfc34
Benjamin <benjminrichie@gmail.com>
2023-10-15 12:46:11 -0700

    handle_print.c update

--- handle_print.c
+++ handle_print.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * handle_print - This function will print argument based on type
+ * @fmt: This is the formatted string to print the arguments.
+ * @list: This is the list of arguments to be printed.
  * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ * @buffer: This is the buffer array to handle print.
+ * @flags: This simply calculates all the active flags
+ * @width: This will get width.
+ * @precision: This is the precision specification
+ * @size: represents the size specifier
+ * Return: 1 or 2
  */
+
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int d, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,11 +24,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	for (d = 0; fmt_types[d].fmt != '\0'; d++)
+		if (fmt[*ind] == fmt_types[d].fmt)
+			return (fmt_types[d].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+	if (fmt_types[d].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
